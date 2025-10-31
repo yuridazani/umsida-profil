a88c1b2b8131884a7da08cd548c5bff6d98cb9cd
Yurida Zani <yuridazani23@gmail.com>
2025-10-31 07:29:42 +0700

    Yurida - Update app_colors, Images & pubspec

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF003366); // Biru Tua UMSIDA
+  static const Color primaryGold = Color(0xFFFDB913); // Emas UMSIDA
+  static const Color lightBackground = Color(0xFFF4F4F4); // Abu-abu muda
+  static const Color darkText = Color(0xFF333333); // Teks gelap
+  static const Color white = Color(0xFFFFFFFF);
+}
